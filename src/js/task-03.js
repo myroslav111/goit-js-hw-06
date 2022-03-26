00c235f4c384edbb68f2e13295a07c4f44faa509
myroslav <myrkozar@gmail.com>
2022-03-26 21:24:27 +0200

    task-3
    task-3 done

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,4 @@
+'use strict'
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,16 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const forGalleryEl = document.querySelector('.gallery')
+
+const mekeListGalleryRowMarkup = (arr) =>{
+const {url, alt} = arr
+
+  return `<li><img class='photo' src= ${url} alt= '${alt}'/></li>`
+}
+
+const listGalleryRowMarkup = images.map(mekeListGalleryRowMarkup).join('')
+
+forGalleryEl.insertAdjacentHTML('afterbegin', listGalleryRowMarkup)
+
