8a44990039b8bdb47e4798aa4b359436a8c228e0
myroslav <myrkozar@gmail.com>
2022-04-02 10:07:34 +0300

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const refsContainer = document.querySelector('#counter')
+console.log(refsContainer);
+const refsBtnDecr = document.querySelector('[data-action="decrement"]')
+const refsBtnIncr = document.querySelector('[data-action="increment"]')
+const refsValue = document.querySelector('#value')
+console.dir(refsValue);
