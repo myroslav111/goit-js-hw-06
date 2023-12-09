3e32512ad759ad4b992315e591cd73b9c96b1ac6
myroslav <myrkozar@gmail.com>
2023-12-09 16:44:11 +0200

    add new smole prj

---
+++ js/task-12.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const searchBarContainerEl = document.querySelector('.search-bar-container');
+const magnifierEl = document.querySelector('.magnifier');
+
+magnifierEl = addEventListener('click', () => {
+  searchBarContainerEl.classList.toggle('active');
+});
