ebdd67431a5f9713d893155eeb857f4429226270
myroslav <myrkozar@gmail.com>
2023-12-11 17:58:15 +0200

    add new prj video html

--- js/task-13.js
+++ js/task-13.js
@@ -1,2 +1,14 @@
-console.log('k');
-console.log('k');
+const trailerContainerEl = document.querySelector('.trailer-container');
+const btnEl = document.querySelector('.btn');
+const btnCloseEl = document.querySelector('.btn-close');
+const videoEl = document.querySelector('video');
+
+btnEl.addEventListener('click', () => {
+  trailerContainerEl.classList.remove('active');
+});
+
+btnCloseEl.addEventListener('click', () => {
+  trailerContainerEl.classList.add('active');
+  videoEl.pause();
+  videoEl.currentTime = 0;
+});
